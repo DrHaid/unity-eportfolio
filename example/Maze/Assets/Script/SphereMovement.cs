38e107e840822434fe5930f45f28cabcd5e14472
DrHaid <40427630+DrHaid@users.noreply.github.com>
2019-05-29 10:38:21 +0200

    Created example project

---
+++ example/Maze/Assets/Script/SphereMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour
+{
+  public float speed;
+  Rigidbody rb;
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = gameObject.GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float vertical = Input.GetAxis("Vertical");
+    float horizontal = Input.GetAxis("Horizontal");
+
+    rb.velocity = new Vector3(horizontal * speed, rb.velocity.y, vertical * speed);
+  }
+}
---
+++ example/Maze/Assets/Script/Target.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+  public GameObject plane;
+  public GameObject start;
+  public GameObject sphere;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetButtonDown("Cancel"))
+    {
+      sphere.transform.position = start.transform.position;
+      plane.GetComponent<Renderer>().material.color = Color.white;
+    }
+  }
+
+  void OnCollisionEnter()
+  {
+    Debug.Log("Trigger");
+    plane.GetComponent<Renderer>().material.color = Color.green;
+  }
+}
